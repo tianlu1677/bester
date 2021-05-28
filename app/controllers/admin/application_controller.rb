87807f3185770fac433b61cf90a1fe2f578b80e1
tianlu1677 <tianlu1677@gmail.com>
2021-05-28 17:43:49 +0800

    fixe

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,4 +9,14 @@ class Admin::ApplicationController < ApplicationController
   protect_from_forgery
   before_action :require_login
   helper_method :attributes, :resource, :resource_class, :show_attributes
+
+
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: current_user.id) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
